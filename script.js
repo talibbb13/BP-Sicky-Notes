d758ee7149b709cea785783b1d1d789f8b6275c9
Talib Raeen <talibraeen13@gmail.com>
2024-04-30 18:07:13 +0530

    updating icon sources and button functionality bug resolve

--- script.js
+++ script.js
@@ -63,11 +63,16 @@ add.addEventListener("click", function () {
 
         noteHeading.innerHTML = inputHeading;
         noteCont.innerHTML = inputCont;
-        noteOpt.src = "E:\\vscode\\img\\flex_box.png";
-        delNote.src = "E:\\vscode\\img\\red_flag.jpeg";
-        shareNote.src = "E:\\vscode\\img\\share.png";
-        copyNote.src = "E:\\vscode\\img\\copy.png";
-        colorNote.src = "E:\\vscode\\img\\color.png";
+        noteOpt.src =
+          "https://www.freeiconspng.com/thumbs/options-icon/options-icon-30.png";
+        delNote.src =
+          "https://static-00.iconduck.com/assets.00/delete-icon-1864x2048-bp2i0gor.png";
+        shareNote.src =
+          "https://cdn3.iconfinder.com/data/icons/virtual-notebook/16/button_share-512.png";
+        copyNote.src =
+          "https://cdn-icons-png.flaticon.com/512/1621/1621635.png";
+        colorNote.src =
+          "https://cdn.icon-icons.com/icons2/1674/PNG/512/colorpalette_111062.png";
 
         note.appendChild(noteHeading);
         note.appendChild(noteCont);
@@ -78,6 +83,10 @@ add.addEventListener("click", function () {
         note.appendChild(colorNote);
         notesCon.appendChild(note);
         createNotes.style.transform = `translate(-50%, -50%) scale(0)`;
+         delNote.style.opacity = "0";
+         shareNote.style.opacity = "0";
+         copyNote.style.opacity = "0";
+         colorNote.style.opacity = "0";
 
         var colors = document.createElement("div")
         var red = document.createElement("div")
@@ -95,11 +104,16 @@ add.addEventListener("click", function () {
         note.appendChild(colors)
 
         noteOpt.addEventListener("click", function () {
+            console.log("note option pe click hua", flag2);
             if (flag2 == true) {
                 delNote.style.right = "24%";
                 shareNote.style.right = "44%";
                 copyNote.style.right = "64%";
                 colorNote.style.right = "83%";
+                          delNote.style.opacity = "1";
+                          shareNote.style.opacity = "1";
+                          copyNote.style.opacity = "1";
+                          colorNote.style.opacity = "1";
                 flag2 = false
             }
             else {
@@ -107,6 +121,10 @@ add.addEventListener("click", function () {
                 shareNote.style.right = "1vw";
                 copyNote.style.right = "1vw";
                 colorNote.style.right = "1vw";
+                delNote.style.opacity = "0";
+                shareNote.style.opacity = "0";
+                copyNote.style.opacity = "0";
+                colorNote.style.opacity = "0";
 
                 colors.querySelectorAll("div").forEach(val => {
                     val.style.transform = "translateY(0%)"
